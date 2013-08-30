05b9a1a036560a9e17c6072e03ffb6b9a19d9056
Idraki Muhamad <idrakimuhamad@gmail.com>
2013-08-30 17:53:30 +0800

    Can commit?

--- client/view/application/application_dashboard.js
+++ client/view/application/application_dashboard.js
@@ -2,3 +2,7 @@
 Template.application_dashboard.tabung_list = function() {
 	return Tabung.find({ creator : Meteor.userId() });
 }
+
+Template.application_dashboard.created = function() {
+	return moment(this.created).format('MMMM Do, YYYY');
+}
